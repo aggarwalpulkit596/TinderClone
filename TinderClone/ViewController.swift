5bb998f13f7f2c0a336e1330bcf63b98f4c7a7dd
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-05-28 11:55:24 +0530

    Add cards animation

--- TinderClone/ViewController.swift
+++ TinderClone/ViewController.swift
@@ -11,29 +11,36 @@ import UIKit
 class ViewController: UIViewController {
     
     let topStackView = TopNavigationStackView()
-    let bottomStackView = HomeBottomControlsStackView()
-    let blueView = UIView()
-
+    let cardsDeckView = UIView()
+    let buttonsStackView = HomeBottomControlsStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        blueView.backgroundColor = .blue
-        
         setupLayout()
-        
+        setupDummyCards()
     }
     
-    //MARK :- Fileprivate
+    fileprivate func setupDummyCards() {
+        print("Setting up dummy cards")
+        let cardView = CardView(frame: .zero)
+        cardsDeckView.addSubview(cardView)
+        cardView.fillSuperview()
+    }
+    
+    // MARK:- Fileprivate
     
     fileprivate func setupLayout() {
-        let overallStackView = UIStackView(arrangedSubviews: [topStackView,blueView,bottomStackView])
+        let overallStackView = UIStackView(arrangedSubviews: [topStackView, cardsDeckView, buttonsStackView])
         overallStackView.axis = .vertical
         view.addSubview(overallStackView)
         overallStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
+        overallStackView.isLayoutMarginsRelativeArrangement = true
+        overallStackView.layoutMargins = .init(top: 0, left: 12, bottom: 0, right: 12)
+        
+        overallStackView.bringSubviewToFront(cardsDeckView)
     }
-
-
+    
 }
+
 
 
---
+++ TinderClone/Views/CardView.swift
@@ -0,0 +1,73 @@
+//
+//  CardView.swift
+//  TinderClone
+//
+//  Created by Pulkit Aggarwal on 28/05/19.
+//  Copyright © 2019 Pulkit Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+    
+    fileprivate let threshold: CGFloat = 80
+
+
+    fileprivate let imageView = UIImageView(image: #imageLiteral(resourceName: "lady5c"))
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        
+        addSubview(imageView)
+        imageView.fillSuperview()
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        addGestureRecognizer(panGesture)
+    }
+    
+    @objc fileprivate func handlePan(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .changed:
+            handleChanged(gesture)
+        case .ended:
+            handleEnded(gesture: gesture)
+        default:
+            ()
+        }
+    }
+    
+    fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: nil)
+        // rotation
+        // some not that scary math here to convert radians to degrees
+        let deg rees: CGFloat = translation.x / 20
+        let angle = degrees * .pi / 180
+        
+        let rotationalTransformation = CGAffineTransform(rotationAngle: angle)
+        self.transform = rotationalTransformation.translatedBy(x: translation.x, y: translation.y)
+    }
+    
+    fileprivate func handleEnded(gesture: UIPanGestureRecognizer) {
+        let translationDirection: CGFloat = gesture.translation(in: nil).x > 0 ? 1 : -1
+        let shouldDismissCard = abs(gesture.translation(in: nil).x) > threshold
+        
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
+            if shouldDismissCard {
+                self.frame = CGRect(x: 1000 * translationDirection, y: 0, width: self.frame.width, height: self.frame.height)
+            } else {
+                self.transform = .identity
+            }
+            
+        }) { (_) in
+            self.transform = .identity
+            self.frame = CGRect(x: 0, y: 0, width: self.superview!.frame.width, height: self.superview!.frame.height)
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
